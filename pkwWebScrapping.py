be82ee516dc71100dd11bb298f97f77fcdcb4d14
Wojciech Sekta <sekta.wojciech@gmail.com>
2023-11-25 01:23:33 +0100

    switch to pyppeteer while scrapping

--- pkwWebScrapping.py
+++ pkwWebScrapping.py
@@ -1,40 +1,41 @@
-import requests
 from bs4 import BeautifulSoup
 import re
-from requests_html import HTMLSession
+import asyncio
+from pyppeteer import launch
 
 def save_to_file(page):
-    f = open("page.html", "wb")
+    f = open("page.html", "w")
     f.write(page)
 
 def get_tab_name(page):
     soup = BeautifulSoup(page, 'html.parser')
     table = soup.findAll(id=re.compile("^DataTables_Table_\d+$"))
+    for tab in table:
+        print(tab.get('id'))
     return table[0].get('id')
 
 def get_district_page(district_number):
-    page_url = f'https://wybory.gov.pl/sejmsenat2023/pl/sejm/wynik/okr/{district_number}'
-    
-    try:
-        session = HTMLSession()
-        response = session.get(page_url)
-        return response.html.render()
-    except requests.exceptions.RequestException as e:
-        print(e)    
+    async def main():
+        browserObj = browser = await launch(
+                headless=True, 
+                args=['--no-sandbox'])
+        url = await browserObj.newPage()
+        await url.goto(f'https://wybory.gov.pl/sejmsenat2023/pl/sejm/wynik/okr/{district_number}')
+
+        await url.waitForNavigation({"waitUntil": 'networkidle0',})
 
 
-district_number = 41
-source = get_district_page(district_number)
-print(source)
-# save_to_file(source)
-# table_name = get_tab_name(source)
-# print(f"Okręg nr. {district_number} ma tabelę o nazwie: {table_name}")
+        ## Get HTML
+        htmlContent = await url.content()
+        await browserObj.close()
+        return htmlContent
 
-# districts = []
-# for district in range(1, 42):
-#     page_url = f'https://wybory.gov.pl/sejmsenat2023/pl/sejm/wynik/okr/{district}'
-#     page = requests.get(page_url)
-#     soup = BeautifulSoup(page.content, 'html.parser')
-    	
-#     table = soup.findAll(id=re.compile("^DataTables_Table_\d+"))
-#     print(f"Okręg nr. {district} ma {table}")
+
+    response = asyncio.get_event_loop().run_until_complete(main())
+    return response
+
+
+for district_number in range(1, 42):
+    source = get_district_page(district_number)
+    table_name = get_tab_name(source)
+    print(f"Okręg nr. {district_number} ma tabelę o nazwie: {table_name}")
